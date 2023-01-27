ae7c5eeb24ca765ebb7566a8ed78d7d2a5a5498e
barisskann <bars_958@hotmail.com>
2023-01-27 13:13:46 +0300

    s9-challenge

--- frontend/components/AppFunctional.js
+++ frontend/components/AppFunctional.js
@@ -1,78 +1,117 @@
-import React from 'react'
+import axios from "axios";
+import React from "react";
+import { useEffect } from "react";
+import { useState } from "react";
 
 // önerilen başlangıç stateleri
-const initialMessage = ''
-const initialEmail = ''
-const initialSteps = 0
-const initialIndex = 4 //  "B" nin bulunduğu indexi
+const initialMessage = "";
+const initialEmail = "";
+const initialSteps = 0;
+const initialIndex = 4; //  "B" nin bulunduğu indexi
 
 export default function AppFunctional(props) {
-  // AŞAĞIDAKİ HELPERLAR SADECE ÖNERİDİR.
-  // Bunları silip kendi mantığınızla sıfırdan geliştirebilirsiniz.
+  const dizi = [0, 1, 2, 3, 4, 5, 6, 7, 8];
+  let sayı = 0;
+  const [click, setClick] = useState(4);
+  const [deger, setDeger] = useState(-1);
+  const [email, setEmail] = useState("");
+  let x = Math.floor(click / 3) + 1;
+  let y = click < 3 ? click + 1 : click + 1 - 3 * (x - 1);
+  const handleEmail = (e) => {
+    setEmail(e.target.value);
+  };
 
-  function getXY() {
-    // Koordinatları izlemek için bir state e sahip olmak gerekli değildir.
-    // Bunları hesaplayabilmek için "B" nin hangi indexte olduğunu bilmek yeterlidir.
-  }
-
-  function getXYMesaj() {
-    // Kullanıcı için "Koordinatlar (2, 2)" mesajını izlemek için bir state'in olması gerekli değildir.
-    // Koordinatları almak için yukarıdaki "getXY" helperını ve ardından "getXYMesaj"ı kullanabilirsiniz.
-    // tamamen oluşturulmuş stringi döndürür.
-  }
-
-  function reset() {
-    // Tüm stateleri başlangıç ​​değerlerine sıfırlamak için bu helperı kullanın.
-  }
-
-  function sonrakiIndex(yon) {
-    // Bu helper bir yön ("sol", "yukarı", vb.) alır ve "B" nin bir sonraki indeksinin ne olduğunu hesaplar.
-    // Gridin kenarına ulaşıldığında başka gidecek yer olmadığı için,
-    // şu anki indeksi değiştirmemeli.
-  }
-
-  function ilerle(evt) {
-    // Bu event handler, "B" için yeni bir dizin elde etmek üzere yukarıdaki yardımcıyı kullanabilir,
-    // ve buna göre state i değiştirir.
-  }
-
-  function onChange(evt) {
-    // inputun değerini güncellemek için bunu kullanabilirsiniz
-  }
-
-  function onSubmit(evt) {
-    // payloadu POST etmek için bir submit handlera da ihtiyacınız var.
-  }
-
+  const handleChange = (e) => {
+    e.preventDefault();
+    axios
+      .post(
+        "http://localhost:9000/api/result",
+        {
+          x,
+          y,
+          steps: deger,
+          email,
+        },
+        {
+         
+        }
+      )
+      .then((err) => console.log(err))
+      .catch((err) => console.log(err));
+  };
+  useEffect(() => {
+    setDeger(deger === -1 ? 0 : deger + 1);
+  }, [click]);
   return (
     <div id="wrapper" className={props.className}>
       <div className="info">
-        <h3 id="coordinates">Koordinatlar (2, 2)</h3>
-        <h3 id="steps">0 kere ilerlediniz</h3>
+        <h3 id="coordinates">
+          Koordinatlar ({x},{y})
+        </h3>
+        <h3 id="steps">{deger} kere ilerlediniz</h3>
       </div>
       <div id="grid">
-        {
-          [0, 1, 2, 3, 4, 5, 6, 7, 8].map(idx => (
-            <div key={idx} className={`square${idx === 4 ? ' active' : ''}`}>
-              {idx === 4 ? 'B' : null}
-            </div>
-          ))
-        }
+        {dizi.map((idx) => (
+          <div key={idx} className={`square${idx === click ? " active" : ""}`}>
+            {idx === click ? "B" : null}
+          </div>
+        ))}
       </div>
       <div className="info">
         <h3 id="message"></h3>
       </div>
       <div id="keypad">
-        <button id="left">SOL</button>
-        <button id="up">YUKARI</button>
-        <button id="right">SAĞ</button>
-        <button id="down">AŞAĞI</button>
-        <button id="reset">reset</button>
+        <button
+          onClick={() => {
+            setClick(click - 3 > -1 ? click - 3 : click);
+          }}
+          id="up"
+        >
+          YUKARI
+        </button>
+        <button
+          onClick={() => {
+            setClick(click + 1 < 9 ? click + 1 : click);
+          }}
+          id="right"
+        >
+          SAĞ
+        </button>
+        <button
+          onClick={() => {
+            setClick(click - 1 > -1 ? click - 1 : click);
+          }}
+          id="left"
+        >
+          SOL
+        </button>
+        <button
+          onClick={() => {
+            setClick(click + 3 < 9 ? click + 3 : click);
+          }}
+          id="down"
+        >
+          AŞAĞI
+        </button>
+        <button
+          onClick={() => {
+            setClick(4);
+            setDeger(deger !== 0 ? -1 : 0);
+          }}
+          id="reset"
+        >
+          reset
+        </button>
       </div>
-      <form>
-        <input id="email" type="email" placeholder="email girin"></input>
+      <form onSubmit={handleChange}>
+        <input
+          id="email"
+          type="email"
+          placeholder="email girin"
+          onChange={handleEmail}
+        ></input>
         <input id="submit" type="submit"></input>
       </form>
     </div>
-  )
+  );
 }
